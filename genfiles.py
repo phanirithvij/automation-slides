792fb5d19512d695d4b63b0b064a732c2dbfe156
Phani Rithvij <phanirithvij2000@gmail.com>
2019-05-02 11:08:19 +0530

    rm cache

--- genfiles.py
+++ genfiles.py
@@ -14,8 +14,9 @@ for roll in ROLLS:
         if e_snake.errno != errno.EEXIST:
             raise OSError
 
-    os.system(f"cp -r src/* {roll}/")                # 1
-    os.system(f"mv {roll}/main.py {roll}/{roll}.py") # 2
+    os.system(f"rm -r src/__pycache__ src/2017*")    # 1
+    os.system(f"cp -r src/* {roll}/")                # 2
+    os.system(f"mv {roll}/main.py {roll}/{roll}.py") # 3
 
-    with open(f"{roll}/config.py", "w+") as config:  # 3 order is important
+    with open(f"{roll}/config.py", "w+") as config:  # 4 order is important
         config.write(f"ROLL = {roll}")
